71fb95eba94360555fed7a68cd743ed89c82a5df
eldy <>
2002-02-23 15:45:48 +0000

    no message

--- wwwroot/cgi-bin/awstats.pl
+++ wwwroot/cgi-bin/awstats.pl
@@ -4497,3 +4497,43 @@ else {
 }
 
 0;	# Do not remove this line
+
+#
+#200 OK
+#201 Created
+#202 Accepted
+#203 Non-Authorative Information
+#204 No Content
+#205 Reset Content
+#206 Partial Content
+#
+#300 Multiple Choices
+#301 Moved Permanently
+#302 Moved Temporarily
+#303 See Other
+#304 Not Modified
+#305 Use Proxy
+#
+#400 Bad Request
+#401 Authorization Required
+#402 Payment Required
+#403 Forbidden
+#404 Not Found
+#405 Method Not Allowed
+#406 Not Acceptable (encoding)
+#407 Proxy Authentication Required
+#408 Request Timed Out
+#409 Conflicting Request
+#410 Gone
+#411 Content Length Required
+#412 Precondition Failed
+#413 Request Entity Too Long
+#414 Request URI Too Long
+#415 Unsupported Media Type
+#
+#500 Internal Server Error
+#501 Not Implemented
+#502 Bad Gateway
+#503 Service Unavailable
+#504 Gateway Timeout
+#505 HTTP Version Not Supported
