f0d44aa4603b53bf23c70d525e2269cc6640d9e1
eldy <>
2003-03-04 22:42:40 +0000

    Added mime lib in exportable lib in awstats_exportlib.pl tool.

--- tools/awstats_exportlib.pl
+++ tools/awstats_exportlib.pl
@@ -67,6 +67,7 @@ use vars qw/
 %OSHashID %OSHashLib
 %RobotsHashIDLib
 %SearchEnginesHashIDLib %SearchEnginesKnownUrl
+%MimeHashFamily %MimeHashLib
 /;
 
 
@@ -210,6 +211,8 @@ if (! $LibToExport || ! $ExportFormat) {
 &Read_Ref_Data($LibToExport);
 
 
+my $libisexportable=0;
+
 # Export data
 #------------
 
@@ -226,6 +229,23 @@ if ($LibToExport =~ /browsers/) {
 			last;
 		}	
 	}
+	$libisexportable=1;
+}
+
+if ($LibToExport =~ /mime/) {
+	foreach my $key (sort keys %MimeHashFamily) {
+		if ($ExportFormat eq 'text') {
+			print "$key\t$MimeHashLib{$MimeHashFamily{$key}}\n";
+		}
+		if ($ExportFormat eq 'webalizer') {
+			print "Webalizer does not support self-defined mime types.\nUse 'text' export format if you want an export list of AWStats Mime types.\n";
+			last;
+		}
+		if ($ExportFormat eq 'analog') {
+			print "TYPEALIAS .$key   \"$key [$MimeHashLib{$MimeHashFamily{$key}}]\"\n";
+		}	
+	}
+	$libisexportable=1;
 }
 
 if ($LibToExport =~ /operating_systems/) {
@@ -243,6 +263,7 @@ if ($LibToExport =~ /operating_systems/) {
 			last;
 		}
 	}
+	$libisexportable=1;
 }
 
 if ($LibToExport =~ /robots/) {
@@ -264,6 +285,7 @@ if ($LibToExport =~ /robots/) {
 			print 'ROBOTINCLUDE '.($robotlist{$key}==1?'':'REGEXPI:')."$key".($robotlist{$key}==1?'*':'')."\n";
 		}	
 	}
+	$libisexportable=1;
 }
 
 if ($LibToExport =~ /search_engines/) {
@@ -281,8 +303,12 @@ if ($LibToExport =~ /search_engines/) {
 			print "SEARCHENGINE http://*$key*/* $urlkeywordsyntax\n";
 		}
 	}
+	$libisexportable=1;
 }
 
+if (! $libisexportable) {
+	print "Export for AWStats lib '$LibToExport' is not supported in this tool version.\n";
+}
 
 
 0;	# Do not remove this line
