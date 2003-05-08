92037a32aba8468af3f52e785821a2b03ba9276a
eldy <>
2003-05-08 14:48:36 +0000

    Small work for a future auto configure script.

--- tools/configure.pl
+++ tools/configure.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-#-Description-------------------------------------------
+#-------------------------------------------------------
 # This script creates one config file for each virtual web servers
 # so AWStats is immedialty working.
 # See COPYING.TXT file about AWStats GNU General Public License.
@@ -14,9 +14,12 @@ use vars qw/ $REVISION $VERSION /;
 $REVISION='$Revision$'; $REVISION =~ /\s(.*)\s/; $REVISION=$1;
 $VERSION="1.0 (build $REVISION)";
 
-# Default value of DIRCONFIG and AWSTATSSCRIPT
-my $DIRCONFIG = "/etc/opt/awstats";
-my $AWSTATSSCRIPT = "/opt/awstats/wwwroot/cgi-bin/awstats.pl";
+use vars qw/
+$DIR $PROG $Extension
+/;
+
+# Possible dirs for Apache conf files
+@WEBCONF=('/usr/local/apache/conf/httpd.conf','/usr/local/apache2/conf/httpd.conf','/etc/httpd/httpd.conf');
 
 
 
@@ -44,21 +47,26 @@ sub debug {
 #-------------------------------------------------------
 # MAIN
 #-------------------------------------------------------
+($DIR=$0) =~ s/([^\/\\]*)$//; ($PROG=$1) =~ s/\.([^\.]*)$//; $Extension=$1;
+
 my $QueryString=""; for (0..@ARGV-1) { $QueryString .= "$ARGV[$_] "; }
 if ($QueryString =~ /debug=/i) { $Debug=$QueryString; $Debug =~ s/.*debug=//; $Debug =~ s/&.*//; $Debug =~ s/ .*//; }
 
 my $helpfound=0;
 for (0..@ARGV-1) {
-	if ($ARGV[$_] =~ /^-*h/i)     		  	 { $helpfound=1; last; }
+	if ($ARGV[$_] =~ /^-*h/i)   { $helpfound=1; last; }
 }
 
 # Show usage help
-my $DIR; my $PROG; my $Extension;
-($DIR=$0) =~ s/([^\/\\]*)$//; ($PROG=$1) =~ s/\.([^\.]*)$//; $Extension=$1;
 if ($helpfound) {
 	print "----- $PROG $VERSION (c) Laurent Destailleur -----\n";
-	print "$PROG creates all AWStats config files for each virtual servers\n";
-	print "found in an Apache web server configuration.\n";
+	print "$PROG is a tool to setup AWStats. It works with Apache only.\n";
+	print "- It detects web server install path\n";
+	print "- It detects global cgi-bin and icons directory\n";
+	print "- It copy cgi and icons files in thoose directories\n";
+	print "- It extract list of virtual servers and create one config file for each of\n";
+	print "  them.\n";
+	print "- It return commands and URL(s) for using Awstats for each config file.\n";
 	print "\n";
 	print "Usage:  $PROG.$Extension\n";
 	print "\n";
@@ -78,41 +86,61 @@ if ($nowsec < 10) { $nowsec = "0$nowsec"; }
 
 
 
-print "This script is not ready yet.\n";
+print "THIS SCRIPT IS NOT READY YET.\n";
 print "See AWStats setup documentation instead (file docs/index.html).\n";
+print "\n";
+print "If you want to help and write this script, run ot with -h option\n";
+print "to known what it should do.\n\n";
 
 
-
-# Search web server
+# Detect web server path
+# ---------------------
 my $ApachePath="";
 
 
 
 
 
-if (! $ApachePath) { exit 1; }
 
 
 
+if (! $ApachePath) {
+	error("Your web server path could not be found.\nIf uou are not using Apache web server, you must setup AWStats manually.\nSee AWStats setup documentation (file docs/index.html)");
+	exit 1;
+}
 
-# Search Apache config file
+
+
+# Open Apache config file
+# -----------------------
+
+# TODO
 
 
 
+# Copy cgi-bin and icons into global cgi-bin and icons directory
+# --------------------------------------------------------------
+
+# TODO
 
 
 # Search virtual servers
+# --------------------------------------------------------------
+
+# TODO
+
+
+# Loop on each virtual servers and create one config file
+# --------------------------------------------------------------
+
+# TODO
 
 
 
+# Loop on each virtual servers and show on screen the URL to use
+# --------------------------------------------------------------
 
-
-# Loop on each virtual servers to create one config file
-
-
-
-
-
+# TODO
 
 
 0;	# Do not remove this line
