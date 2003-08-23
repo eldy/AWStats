c480f40c8cb842a5e6cacdba3f370d1913fe7fef
eldy <>
2003-08-23 10:56:01 +0000

    Updated documentation.

--- wwwroot/cgi-bin/lib/domains.pm
+++ wwwroot/cgi-bin/lib/domains.pm
@@ -58,7 +58,7 @@
 "ky","Cayman Islands","kz","Kazakhstan","la","Laos","lb","Lebanon","lc","Saint Lucia",
 "li","Liechtenstein","lk","Sri Lanka","lr","Liberia","ls","Lesotho","lt","Lithuania",
 "lu","Luxembourg","lv","Latvia","ly","Libya","ma","Morocco","mc","Monaco",
-"md","Moldavia","mg","Madagascar","mh","Marshall Islands","mil","USA Military",
+"md","Moldova","mg","Madagascar","mh","Marshall Islands","mil","USA Military",
 "mk","Macedonia","ml","Mali","mm","Myanmar","mn","Mongolia","mo","Macau",
 "mp","Northern Mariana Islands","mq","Martinique (French)","mr","Mauritania",
 "ms","Montserrat","mt","Malta","mu","Mauritius","museum","Museum domains","mv","Maldives",
