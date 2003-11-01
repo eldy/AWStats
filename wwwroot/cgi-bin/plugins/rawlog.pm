fc278b04ab130878256e56b970dfba3fd7169920
eldy <>
2003-11-01 02:48:14 +0000

    Litlle support for right to left languages.

--- wwwroot/cgi-bin/plugins/rawlog.pm
+++ wwwroot/cgi-bin/plugins/rawlog.pm
@@ -109,8 +109,8 @@ sub _ShowForm {
 	print "<table class=\"aws_border\" border=\"0\" cellpadding=\"2\" cellspacing=\"0\" width=\"100%\">\n";
 	print "<tr><td>";
 	print "<table class=\"aws_data\" border=\"0\" cellpadding=\"1\" cellspacing=\"0\" width=\"100%\">\n";
-	print "<tr align=\"left\"><td align=\"left\"><b>Show content of file '$LogFile' ($MAXLINE first lines):</b></td></tr>\n";
-	print "<tr align=\"left\"><td align=\"left\">$Message[79]: <input type=\"text\" name=\"filterrawlog\" value=\"$Filter\" /><input type=\"submit\" value=\"List\" class=\"aws_button\" />\n";
+	print "<tr><td><b>Show content of file '$LogFile' ($MAXLINE first lines):</b></td></tr>\n";
+	print "<tr><td>$Message[79]: <input type=\"text\" name=\"filterrawlog\" value=\"$Filter\" /><input type=\"submit\" value=\"List\" class=\"aws_button\" />\n";
 	print "<input type=\"hidden\" name=\"config\" value=\"$SiteConfig\" /><input type=\"hidden\" name=\"framename\" value=\"$FrameName\" /><input type=\"hidden\" name=\"pluginmode\" value=\"rawlog\" />";
 	print "</td></tr>\n";
 	print "</table>\n";
