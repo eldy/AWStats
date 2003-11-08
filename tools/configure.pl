d68d8268af7a03864cbeaf765c1698338842f2a7
eldy <>
2003-11-08 17:47:37 +0000

    Better installer for windows user.

--- tools/configure.pl
+++ tools/configure.pl
@@ -1,10 +1,15 @@
 #!/usr/bin/perl
 #-------------------------------------------------------
-# This script creates one config file for each virtual web servers
-# so AWStats is immedialty working.
+# This script configures AWStats on a Windows OS so AWStats
+# is immediately working:
+# - Get Apache config file from registry (ask if not found)
+# - Add AWStats directives
+# - Change common to combined (ask to confirm)
 # See COPYING.TXT file about AWStats GNU General Public License.
 #-------------------------------------------------------
 # $Revision$ - $Author$ - $Date$
+use strict;
+use Win32::TieRegistry;
 
 
 #-------------------------------------------------------
@@ -15,12 +20,25 @@ $REVISION='$Revision$'; $REVISION =~ /\s(.*)\s/; $REVISION=$1;
 $VERSION="1.0 (build $REVISION)";
 
 use vars qw/
-$DIR $PROG $Extension
+$DIR $PROG $Extension $Debug
 /;
 
+use vars qw/
+@WEBCONF
+/;
 # Possible dirs for Apache conf files
-@WEBCONF=('/usr/local/apache/conf/httpd.conf','/usr/local/apache2/conf/httpd.conf','/etc/httpd/httpd.conf');
+@WEBCONF=(
+'C:/Program Files/Apache Group/Apache2/conf/httpd.conf',
+'C:/Program Files/Apache Group/Apache/conf/httpd.conf',
+'/etc/httpd/httpd.conf',
+'/usr/local/apache/conf/httpd.conf',
+'/usr/local/apache2/conf/httpd.conf'
+);
 
+use vars qw/
+$WebServerChanged
+/;
+$WebServerChanged=0;
 
 
 
@@ -53,20 +71,26 @@ my $QueryString=""; for (0..@ARGV-1) { $QueryString .= "$ARGV[$_] "; }
 if ($QueryString =~ /debug=/i) { $Debug=$QueryString; $Debug =~ s/.*debug=//; $Debug =~ s/&.*//; $Debug =~ s/ .*//; }
 
 my $helpfound=0;
+my $AWSTATSPATH;
+my $OS='';
+my $CR='';
 for (0..@ARGV-1) {
-	if ($ARGV[$_] =~ /^-*h/i)   { $helpfound=1; last; }
+	if ($ARGV[$_] =~ /^-*h/i)   					{ $helpfound=1; last; }
+	if ($ARGV[$_] =~ /^-*awstatspath=([^\s\"]+)/i)  { $AWSTATSPATH==$1; last; }
+}
+if (! $AWSTATSPATH) {
+	$AWSTATSPATH="$DIR";
+	$AWSTATSPATH=~s/tools[\\\/]?$//;
+	$AWSTATSPATH=~s/[\\\/]$//;
 }
 
 # Show usage help
 if ($helpfound) {
 	print "----- $PROG $VERSION (c) Laurent Destailleur -----\n";
 	print "$PROG is a tool to setup AWStats. It works with Apache only.\n";
-	print "- It detects web server install path\n";
-	print "- It detects global cgi-bin and icons directory\n";
-	print "- It copy cgi and icons files in thoose directories\n";
-	print "- It extract list of virtual servers and create one config file for each of\n";
-	print "  them.\n";
-	print "- It return commands and URL(s) for using Awstats for each config file.\n";
+	print "- Get Apache config file from registry (ask if not found)\n";
+	print " - Add AWStats directives\n";
+	print " - Change common to combined (ask to confirm)\n";
 	print "\n";
 	print "Usage:  $PROG.$Extension\n";
 	print "\n";
@@ -84,63 +108,158 @@ if ($nowhour < 10) { $nowhour = "0$nowhour"; }
 if ($nowmin < 10) { $nowmin = "0$nowmin"; }
 if ($nowsec < 10) { $nowsec = "0$nowsec"; }
 
-
-
-print "THIS SCRIPT IS NOT READY YET.\n";
-print "See AWStats setup documentation instead (file docs/index.html).\n";
-print "\n";
-print "If you want to help and write this script, run ot with -h option\n";
-print "to known what it should do.\n\n";
-
+# Detect web server path
+# ----------------------
+if (-d "/etc" && -d "/home") { $OS='linux'; $CR=''; }
+else { $OS='windows'; $CR="\r"; }
+#print "Running OS detected: $OS (Perl $^[)\n";
+print "Running OS detected: $OS\n";
 
 # Detect web server path
-# ---------------------
-my $ApachePath="";
+# ----------------------
+print "Check for web server install...\n";
+my %ApachePath=();		# All Apache path found
+my %ApacheConfPath=();	# All Apache config found
+my $tips;
+if ($OS eq 'linux') {
+	my $found=0;
+	foreach my $conf (@WEBCONF) {
+		if (-s "$conf") {
+			print " Found Web server Apache config file '$conf'\n";
+			$ApacheConfPath{"$conf"}=++$found;
+		}
+	}
+}
+if ($OS eq 'windows') {
+	$Registry->Delimiter("/");
+	if ($tips=$Registry->{"LMachine/Software/Apache Group/Apache/"}) {
+		# If Apache registry call successfull
+		my $found=0;
+		foreach( sort keys %$tips  ) {
+			my $path=$Registry->{"LMachine/Software/Apache Group/Apache/$_/ServerRoot"};
+			$path=~s/[\\\/]$//;
+			if (-d "$path" && -s "$path/conf/httpd.conf") {
+				print " Found a Web server Apache install in '$path'\n";
+				$ApachePath{"$path"}=++$found;
+				$ApacheConfPath{"$path/conf/httpd.conf"}=++$found;
+			}
+		}
+	}
+}
 
-
-
-
-
-
-
-
-if (! $ApachePath) {
-	error("Your web server path could not be found.\nIf uou are not using Apache web server, you must setup AWStats manually.\nSee AWStats setup documentation (file docs/index.html)");
+if (! scalar keys %ApacheConfPath) {
+	error("Your web server config file(s) could not be found.\nIf you are not using Apache web server, you must setup AWStats manually.\nSee AWStats setup documentation (file docs/index.html)");
 	exit 1;
 }
 
-
-
 # Open Apache config file
 # -----------------------
+foreach my $key (keys %ApacheConfPath) {
+	print "Check and complete web server config file '$key'...\n";
+	# Read config file to search for awstats directives
+	open(CONF,"<$key") || error("Failed to open config file '$key' for reading");
+	binmode CONF;
+	my $awstatsjsfound=0;
+	my $awstatsclassesfound=0;
+	my $awstatscssfound=0;
+	my $awstatsiconsfound=0;
+	my $awstatscgifound=0;
+	while(<CONF>) {
+		if ($_ =~ /Alias \/awstatsjs/) 			{ $awstatsjsfound=1; }
+		if ($_ =~ /Alias \/awstatsclasses/) 	{ $awstatsclassesfound=1; }
+		if ($_ =~ /Alias \/awstatscss/) 		{ $awstatscssfound=1; }
+		if ($_ =~ /Alias \/awstatsicons/) 		{ $awstatsiconsfound=1; }
+		if ($_ =~ /ScriptAlias \/awstats\//)	{ $awstatscgifound=1; }
+	}	
+	close CONF;
+
+	if ($awstatsjsfound && $awstatsclassesfound && $awstatscssfound && $awstatsiconsfound && $awstatscgifound) {
+		print " Config file '$key' already setup\n";
+		next;
+	}
+	
+	# Add awstats directives
+	open(CONF,">>$key") || error("Failed to open config file '$key' for adding AWStats directives");
+		binmode CONF;
+		print CONF "$CR\n";
+		print CONF "#$CR\n";
+		print CONF "# Directives to allow use of AWStats as a CGI$CR\n";
+		print CONF "#$CR\n";
+		if (! $awstatsjsfound) {
+			print " Add 'Alias \/awstatsjs \"$AWSTATSPATH/wwwroot/js/\"' to config file\n";
+			print CONF "Alias \/awstatsjs \"$AWSTATSPATH/wwwroot/js/$CR\n";
+		}
+		if (! $awstatsclassesfound) {
+			print " Add 'Alias \/awstatsclasses \"$AWSTATSPATH/wwwroot/classes/\"' to config file\n";
+			print CONF "Alias \/awstatsclasses \"$AWSTATSPATH/wwwroot/classes/$CR\n";
+		}
+		if (! $awstatscssfound) {
+			print " Add 'Alias \/awstatscss \"$AWSTATSPATH/wwwroot/css/\"' to config file\n";
+			print CONF "Alias \/awstatscss \"$AWSTATSPATH/wwwroot/css/$CR\n";
+		}
+		if (! $awstatsiconsfound) {
+			print " Add 'Alias \/awstatsicons \"$AWSTATSPATH/wwwroot/icon/\"' to config file\n";
+			print CONF "Alias \/awstatsicons \"$AWSTATSPATH/wwwroot/icon/$CR\n";
+		}
+		if (! $awstatscgifound) {
+			print " Add 'ScriptAlias \/awstats\/ \"$AWSTATSPATH/wwwroot/cgi-bin/\"' to config file\n";
+			print CONF "ScriptAlias \/awstats\/ \"$AWSTATSPATH/wwwroot/cgi-bin/$CR\n";
+		}
+	close CONF;
+	$WebServerChanged=1;
+}
+
+# Define config file path
+# -----------------------
+my $configfile='';
+if ($OS eq 'linux') 	{ $configfile='/etc/awstats/awstats.conf'; }
+if ($OS eq 'windows') 	{ $configfile="$AWSTATSPATH\\wwwroot\\cgi-bin\\awstats.conf"; }
+
+# Create awstats.conf file
+# -----------------------
+print "Create config file '$configfile' for main site\n";
+
+if (-s $configfile) { print " Config file already exists. No change made.\n"; }
+else {
+
+
+}
+
+# Restart Apache if change were made
+# ----------------------------------
+if ($WebServerChanged) {
+	if ($OS eq 'linux') 	{
+	 	my $ret=`/usr/bin/service httpd restart`;
+	}
+	if ($OS eq 'windows')	{
+		foreach my $key (keys %ApachePath) {
+			if (-f "$key/bin/Apache.exe") {
+				print "Restart Apache with '\"$key/bin/Apache.exe\" -k restart'\n";
+			 	my $ret=`"$key/bin/Apache.exe" -k restart`;
+			}
+		}
+	}
+}
+
 
 # TODO
+# Scan logorate for a log file
+# If apache log has a logrotate log found, we create a config and add line in prerotate
+# prerotate
+#   ...
+# endscript
+# If not found
 
-
-
-# Copy cgi-bin and icons into global cgi-bin and icons directory
-# --------------------------------------------------------------
 
 # TODO
+# Ask to run awstats update process
 
 
-# Search virtual servers
-# --------------------------------------------------------------
-
-# TODO
-
-
-# Loop on each virtual servers and create one config file
-# --------------------------------------------------------------
-
-# TODO
-
-
-
-# Loop on each virtual servers and show on screen the URL to use
-# --------------------------------------------------------------
-
-# TODO
+my $bidon;
+print "\n";
+print "You can send read your statistics with the following URL:\n";
+print "http://localhost/awstats/awstats.pl\n";
+read STDIN, $bidon, 1;
 
 
 0;	# Do not remove this line
