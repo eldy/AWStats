497112a45a8c1d642c57e7be2d2cf1b4d3ce0ee8
eldy <>
2004-01-17 02:19:42 +0000

    Renamed "nimba" to real name "nimda"

--- wwwroot/cgi-bin/lib/worms.pm
+++ wwwroot/cgi-bin/lib/worms.pm
@@ -34,12 +34,12 @@
 %WormsHashID = (
 '\/default\.ida','code_red',
 '\/null\.idq','code_red',
-'exe\?\/c\+dir','nimba',
-'root\.exe','nimba',
-'admin\.dll','nimba',
+'exe\?\/c\+dir','nimda',
+'root\.exe','nimda',
+'admin\.dll','nimda',
 '\/nsiislog\.dll','mpex',
 '\/sumthin','sumthin',
-'\/winnt\/system32\/cmd\.exe','nimba',
+'\/winnt\/system32\/cmd\.exe','nimda',
 '\/_vti_inf\.html','unknown',
 '\/_vti_bin\/shtml\.exe\/_vti_rpc','unknown'
 #'/MSOffice/cltreq.asp'		# Not a worm, a check by IE to see if discussion bar is turned on
@@ -53,7 +53,7 @@
 %WormsHashLib = (
 'code_red','Code Red family worm',
 'mpex','IIS Exploit worm',
-'nimba','Nimba family worm',
+'nimda','Nimda family worm',
 'sumthin','Sumthin worm',
 'unknown','Unknown worm'
 );
