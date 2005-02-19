afeb51e41fdf8ad90f9e1840b621f99089a35ee2
eldy <>
2005-02-19 21:03:18 +0000

    Add makepack-awstats.pl script

--- make/makepack-awstats.pl
+++ make/makepack-awstats.pl
@@ -125,22 +125,23 @@ foreach my $target (keys %CHOOSEDTARGET) {
     foreach my $req (split(/[,\s]/,$REQUIREMENTTARGET{$target})) {
         # Test    
         print "Test requirement for target $target: Search '$req'... ";
-        $ret1=`"$req" 2>&1`;
+        $ret=`"$req" 2>&1`;
         $coderetour=$?; $coderetour2=$coderetour>>8;
-        if ($coderetour != 0 && $coderetour2 == 1 && $PROGPATH) { 
-            # If error not found, we try in PROGPATH
-            $ret2=`"$PROGPATH/$ALTERNATEPATH{$req}/$req\" 2>&1`;
+        if ($coderetour != 0 && (($coderetour2 == 1 && $OS =~ /windows/ && $ret !~ /Usage/i) || ($coderetour2 == 127 && $OS !~ /windows/)) && $PROGPATH) { 
+            # Not found error, we try in PROGPATH
+            $ret=`"$PROGPATH/$ALTERNATEPATH{$req}/$req\" 2>&1`;
             $coderetour=$?; $coderetour2=$coderetour>>8;
             $REQUIREMENTTARGET{$target}="$PROGPATH/$ALTERNATEPATH{$req}/$req";
         }    
 
-        if ($coderetour == 0 || $coderetour2 > 1 || $ret1 =~ /Usage/im || $ret2 =~ /Usage/im) {
-            # Pas erreur ou erreur autre que programme absent
-            print " Found ".$REQUIREMENTTARGET{$target}."\n";
-        } else {
+        if ($coderetour != 0 && (($coderetour2 == 1 && $OS =~ /windows/ && $ret !~ /Usage/i) || ($coderetour2 == 127 && $OS !~ /windows/))) {
+            # Not found error
             print "Not found\nCan't build target $target. Requirement '$req' not found in PATH\n";
             $CHOOSEDTARGET{$target}=-1;
             last;
+        } else {
+            # Pas erreur ou erreur autre que programme absent
+            print " Found ".$REQUIREMENTTARGET{$target}."\n";
         }
     }
 }
