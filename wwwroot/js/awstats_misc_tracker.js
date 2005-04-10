6aa94f00b885740b28c2f1a98c928d013812de8f
eldy <>
2005-04-10 15:41:32 +0000

    Fix browsers compatibility errors in js script

--- wwwroot/js/awstats_misc_tracker.js
+++ wwwroot/js/awstats_misc_tracker.js
@@ -3,8 +3,8 @@
 // You can add this file onto some of your web pages (main home page can
 // be enough) by adding the following HTML code to your page body:
 //
-// <script language=javascript src="/js/awstats_misc_tracker.js"></script>
-// <noscript><img src="/js/awstats_misc_tracker.js?nojs=y" height=0 width=0 border=0 style="display: none"></noscript>
+// <script language=javascript type="text/javascript" src="/js/awstats_misc_tracker.js"></script>
+// <noscript><img src="/js/awstats_misc_tracker.js?nojs=y" height="0" width="0" border="0" style="display: none"></noscript>
 //
 // * This must be added after the <body> tag, not placed within the
 //   <head> tags, or the resulting tracking <img> tag will not be handled
@@ -36,21 +36,21 @@ function awstats_setCookie(TRKNameOfCookie, TRKvalue, TRKexpirehours) {
 }
 
 function awstats_detectIE(TRKClassID) {
-	TRKresult = false;
+	var TRKresult = false;
 	document.write('<SCR' + 'IPT LANGUAGE=VBScript>\n on error resume next \n TRKresult = IsObject(CreateObject("' + TRKClassID + '"))</SCR' + 'IPT>\n');
 	if (TRKresult) return 'y';
 	else return 'n';
 }
 
 function awstats_detectNS(TRKClassID) {
-	TRKn = "n";
+	var TRKn = "n";
 	if (TRKnse.indexOf(TRKClassID) != -1) if (navigator.mimeTypes[TRKClassID].enabledPlugin != null) TRKn = "y";
 	return TRKn;
 }
 
 function awstats_getCookie(TRKNameOfCookie){
 	if (document.cookie.length > 0){
-		TRKbegin = document.cookie.indexOf(TRKNameOfCookie+"=");
+		var TRKbegin = document.cookie.indexOf(TRKNameOfCookie+"=");
 	    if (TRKbegin != -1) {
 			TRKbegin += TRKNameOfCookie.length+1; 
 			TRKend = document.cookie.indexOf(";", TRKbegin);
@@ -64,16 +64,16 @@ function awstats_getCookie(TRKNameOfCookie){
 
 if (window.location.search == "" || window.location.search == "?") {
     // If no query string
-	TRKnow = new Date();
-	TRKscreen=screen.width+"x"+screen.height;
-	if (navigator.appName != "Netscape") {TRKcdi=screen.colorDepth}
+	var TRKnow = new Date();
+	var TRKscreen=screen.width+"x"+screen.height;
+	if (navigator.appName != "Netscape") { TRKcdi=screen.colorDepth; }
 	else {TRKcdi=screen.pixelDepth};
-	TRKjava=navigator.javaEnabled();
-	TRKuserid=awstats_getCookie("AWSUSER_ID");
-	TRKsessionid=awstats_getCookie("AWSSESSION_ID");
+	var TRKjava=navigator.javaEnabled();
+	var TRKuserid=awstats_getCookie("AWSUSER_ID");
+	var TRKsessionid=awstats_getCookie("AWSSESSION_ID");
 	var TRKrandomnumber=Math.floor(Math.random()*10000);
-	if (TRKuserid == null || (TRKuserid=="")) {TRKuserid = "awsuser_id" + TRKnow.getTime() +"r"+ TRKrandomnumber};
-	if (TRKsessionid == null || (TRKsessionid=="")) {TRKsessionid = "awssession_id" + TRKnow.getTime() +"r"+ TRKrandomnumber};
+	if (TRKuserid == null || (TRKuserid=="")) { TRKuserid = "awsuser_id" + TRKnow.getTime() +"r"+ TRKrandomnumber; }
+	if (TRKsessionid == null || (TRKsessionid=="")) { TRKsessionid = "awssession_id" + TRKnow.getTime() +"r"+ TRKrandomnumber; }
 	awstats_setCookie("AWSUSER_ID", TRKuserid, 10000);
 	awstats_setCookie("AWSSESSION_ID", TRKsessionid, 1);
 	TRKuserid=""; TRKuserid=awstats_getCookie("AWSUSER_ID");
@@ -86,33 +86,52 @@ if (window.location.search == "" || window.location.search == "?") {
 	var TRKmac = (TRKagt.indexOf("mac")!=-1);
 
     // Detect the browser internal width and height
+    var TRKwinsize;
     if (document.documentElement && document.documentElement.clientWidth)
         TRKwinsize = document.documentElement.clientWidth + 'x' + document.documentElement.clientHeight;
-    else if (document.body)
+    else if (document.body && document.body.clientWidth)
         TRKwinsize = document.body.clientWidth + 'x' + document.body.clientHeight;
     else
         TRKwinsize = window.innerWidth + 'x' + window.innerHeight;
 	
+	var TRKshk;
+	var TRKfla;
+	var TRKrp;
+	var TRKmov;
+	var TRKwma;
+	var TRKpdf;
+
 	if (TRKie && TRKwin) {
-		var TRKshk = awstats_detectIE("SWCtl.SWCtl.1")
-		var TRKfla = awstats_detectIE("ShockwaveFlash.ShockwaveFlash.1")
-		var TRKrp  = awstats_detectIE("rmocx.RealPlayer G2 Control.1")
-		var TRKmov = awstats_detectIE("QuickTimeCheckObject.QuickTimeCheck.1")
-		var TRKwma = awstats_detectIE("MediaPlayer.MediaPlayer.1")
-		var TRKpdf = 'n'; 
+		TRKshk = awstats_detectIE("SWCtl.SWCtl.1")
+		TRKfla = awstats_detectIE("ShockwaveFlash.ShockwaveFlash.1")
+		TRKrp  = awstats_detectIE("rmocx.RealPlayer G2 Control.1")
+		TRKmov = awstats_detectIE("QuickTimeCheckObject.QuickTimeCheck.1")
+		TRKwma = awstats_detectIE("MediaPlayer.MediaPlayer.1")
+		TRKpdf = 'n'; 
         if (awstats_detectIE("PDF.PdfCtrl.1") == 'y') { TRKpdf = 'y'; }
 	    if (awstats_detectIE('PDF.PdfCtrl.5') == 'y') { TRKpdf = 'y'; }
 		if (awstats_detectIE('PDF.PdfCtrl.6') == 'y') { TRKpdf = 'y'; }
 	}
 	if (TRKns || !TRKwin) {
-		TRKnse = ""; for (var TRKi=0;TRKi<navigator.mimeTypes.length;TRKi++) TRKnse += navigator.mimeTypes[TRKi].type.toLowerCase();
-		var TRKshk = awstats_detectNS("application/x-director")
-		var TRKfla = awstats_detectNS("application/x-shockwave-flash")
-		var TRKrp  = awstats_detectNS("audio/x-pn-realaudio-plugin")
-		var TRKmov = awstats_detectNS("video/quicktime")
-		var TRKwma = awstats_detectNS("application/x-mplayer2")
-		var TRKpdf = awstats_detectNS("application/pdf");
+		var TRKnse = ""; for (var TRKi=0;TRKi<navigator.mimeTypes.length;TRKi++) TRKnse += navigator.mimeTypes[TRKi].type.toLowerCase();
+		TRKshk = awstats_detectNS("application/x-director")
+		TRKfla = awstats_detectNS("application/x-shockwave-flash")
+		TRKrp  = awstats_detectNS("audio/x-pn-realaudio-plugin")
+		TRKmov = awstats_detectNS("video/quicktime")
+		TRKwma = awstats_detectNS("application/x-mplayer2")
+		TRKpdf = awstats_detectNS("application/pdf");
 	}
-	document.write('<img src="'+awstatsmisctrackerurl+'?screen='+TRKscreen+'&win='+TRKwinsize+'&cdi='+TRKcdi+'&java='+TRKjava+'&shk='+TRKshk+'&fla='+TRKfla+'&rp='+TRKrp+'&mov='+TRKmov+'&wma='+TRKwma+'&pdf='+TRKpdf+'&uid='+TRKuserid+'&sid='+TRKsessionid+'" height=0 width=0 border=0>')
+
+	var imgsrc = awstatsmisctrackerurl+'?screen='+TRKscreen+'&win='+TRKwinsize+'&cdi='+TRKcdi+'&java='+TRKjava+'&shk='+TRKshk+'&fla='+TRKfla+'&rp='+TRKrp+'&mov='+TRKmov+'&wma='+TRKwma+'&pdf='+TRKpdf+'&uid='+TRKuserid+'&sid='+TRKsessionid;
+	if( document.createElementNS ) {
+    	var l=document.createElementNS("http://www.w3.org/1999/xhtml","img");
+        l.setAttribute("src", imgsrc );
+        l.setAttribute("height", "0");
+        l.setAttribute("width", "0");
+        l.setAttribute("border", "0");
+        document.getElementsByTagName("body")[0].appendChild(l);
+	} else {
+		document.write('<img src="'+ imgsrc +'" height=\"0\" width=\"0\" border=\"0\" />')
+	}
 
 }
