5658739e31ff7e37875241d17578b8e4e8a5240d
eldy <>
2005-05-05 17:58:18 +0000

    Renamed european union by european country

--- wwwroot/cgi-bin/lib/domains.pm
+++ wwwroot/cgi-bin/lib/domains.pm
@@ -40,7 +40,7 @@
 'dj','Djibouti','dk','Denmark','dm','Dominica','do','Dominican Republic',
 'dz','Algeria','ec','Ecuador','edu','USA Educational','ee','Estonia',
 'eg','Egypt','eh','Western Sahara','er','Eritrea','es','Spain','et','Ethiopia',
-'eu','European Union',
+'eu','European country',
 'fi','Finland','fj','Fiji','fk','Falkland Islands','fm','Micronesia','fo','Faroe Islands',
 'fr','France','fx','France (European Territory)','ga','Gabon',
 'gb','Great Britain','gd','Grenada','ge','Georgia','gf','French Guyana',
