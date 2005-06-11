68aff0a4a276c75517cf76550a5f6890429ebf22
eldy <>
2005-06-11 17:57:52 +0000

    Fixed: Version for webmin module not up to date

--- make/makepack-awstats_webmin.pl
+++ make/makepack-awstats_webmin.pl
@@ -10,7 +10,7 @@ use Cwd;
 
 $PROJECT="awstats";
 $MAJOR="1";
-$MINOR="5";
+$MINOR="6";
 @LISTETARGET=("WBM");   # Possible packages
 %REQUIREMENTTARGET=(                            # Tool requirement for each package
 "WBM"=>"tar",
