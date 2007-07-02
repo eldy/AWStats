fe8f01b71a997977faa3c5c98f4cac0b5e2ce7a5
cvs2git <admin@example.com>
2007-07-02 22:24:22 +0000

    This commit was manufactured by cvs2git to create tag 'AWSTATS_6_7'.
    Sprout from master 2007-07-02 22:24:21 UTC eldy 'Doc: Updated documentation'
    Cherrypick from master 2007-03-06 18:35:20 UTC eldy 'Added option NotPageList':
        tools/awstats_buildstaticpages.pl
        wwwroot/cgi-bin/awstats.pl

--- wwwroot/cgi-bin/awstats.pl
+++ wwwroot/cgi-bin/awstats.pl
@@ -7791,7 +7791,6 @@ if (scalar keys %HTMLOutput) {
 			}
 			else {
 				print "<span style=\"font-size: 14px;\">";
-				if ($DayRequired) { print "$Message[4] $DayRequired - "; }	
 				if ($MonthRequired eq 'all') { print "$Message[6] $YearRequired"; }
 				else { print "$Message[5] $MonthNumLib{$MonthRequired} $YearRequired"; }
 				print "</span>";
