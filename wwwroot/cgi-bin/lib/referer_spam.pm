f582d049591243ccb0636c556c1ec1254bf7d3a7
eldy <>
2007-10-15 02:33:53 +0000

    Add referer spam list

---
+++ wwwroot/cgi-bin/lib/referer_spam.pm
@@ -0,0 +1,49 @@
+# AWSTATS REFERER SPAMMERS ADATABASE
+#-------------------------------------------------------
+# If you want to extend AWStats detection capabilities,
+# you must add an entry in RefererSpamKeys
+#-------------------------------------------------------
+# $Revision$ - $Author$ - $Date$
+
+
+#package AWSREFSPAMMERS;
+
+
+
+# RefererSpamKeys
+# This list is used to know which keywords to search for in referer URLs
+# to find if hits comes from a referer spammers. If referer URLs has a
+# cost higher or equal to 4, it's a referer spammer.
+# key, cost
+#-------------------------------------------------------
+%RefererSpamKeys = (
+'adult'=>1,
+'anal'=>2,
+'dick'=>1,
+'erotic'=>2,			# erotic, erotica
+'gay'=>2,
+'lesbian'=>2,
+'free'=>1,
+'porn'=>2,
+'sex'=>2,
+
+'full-list.net'=>4,
+'voodoomachine.com'=>4,
+'mastodonte.com'=>4,
+'surfnomore.com'=>4,
+'raverpussies.com'=>4,
+'quiveringfuckholes.com'=>4,
+'burningbush.netfirms.com'=>4,
+'lesbo-tennie-girls.lesbian-hardcore-porn-teen-pics.com'=>4,
+'free-people-search-engines.com'=>4,
+'iaea.org'=>4,
+'1stchoicecolo.com'=>4,
+'globoads.com'=>4,
+'morganindustriesinc.com'=>4,
+'chicagodrugclub.com'=>4,
+'massivecocks.com'=>4,
+
+);
+
+
+1;
