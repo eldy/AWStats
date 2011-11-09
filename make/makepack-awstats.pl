12bb260907ceafe995dbf12d0bfdcf45515b8fbc
eldy <>
2011-11-09 14:20:46 +0000

    Prepare 7.1 version

--- make/makepack-awstats.pl
+++ make/makepack-awstats.pl
@@ -3,14 +3,14 @@
 # \file         make/makepack-awstats.pl
 # \brief        Package builder (tgz, zip, rpm, deb, exe)
 # \version      $Revision$
-# \author       (c)2004-2005 Laurent Destailleur  <eldy@users.sourceforge.net>
+# \author       (c)2004-2011 Laurent Destailleur  <eldy@users.sourceforge.net>
 #----------------------------------------------------------------------------
 
 use Cwd;
 
 $PROJECT="awstats";
 $MAJOR="7";
-$MINOR="0";
+$MINOR="1";
 $RPMSUBVERSION="1";
 
 @LISTETARGET=("TGZ","ZIP","RPM","DEB","EXE");   # Possible packages
