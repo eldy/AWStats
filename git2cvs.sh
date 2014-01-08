5db4540c94f3a2f97bbd34136f4cc6c8bdf99b5e
Laurent Destailleur <eldy@destailleur.fr>
2014-01-08 11:34:40 +0100

    Solve pb with cvs

--- git2cvs.sh
+++ git2cvs.sh
@@ -5,8 +5,8 @@
 cvs update
 #cvs udpate -C
 
-
+export LASTINCLUDEDID=6ddc897cd021ad61cc5661297abe61d04e8c9520
 export GIT_DIR=/home/ldestailleur/git/awstats/.git
 
-git-cvsexportcommit IDCOMMIT
+git cherry $LASTINCLUDEDID HEAD | sed -n 's/^+ //p' | xargs -l1 git cvsexportcommit -c -p -v
 
