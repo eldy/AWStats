a8ae707fcb5d436e66d8f6ab7ec6c3f752003379
Laurent Destailleur <eldy@destailleur.fr>
2014-07-26 16:53:01 +0200

    More detail into git2vcs script

--- git2cvs.sh
+++ git2cvs.sh
@@ -7,12 +7,14 @@ cvs update
 #cvs udpate -C
 
 
-# Set here id of last commit
-export LASTINCLUDEDID=eea6adb633b5b055b663bafcfef6e8482aeedad4
+# Set here id of last commit found into git repo of awstats that were included into cvs
+export LASTINCLUDEDID=89ac100831d627ac012e4972af9cc572297de97d
 # Set here where is store git repository
 export GIT_DIR=/home/ldestailleur/git/awstats/.git
 
-
 # Commit 5 commits at once after last commit
-git cherry $LASTINCLUDEDID HEAD | sed -n 's/^+ //p' | head -n 5 | xargs -l1 git cvsexportcommit -c -p -v
+echo "git cherry $LASTINCLUDEDID HEAD | sed -n 's/^+ //p' | head -n 50"
+git cherry $LASTINCLUDEDID HEAD | sed -n 's/^+ //p' | head -n 50
+
+git cherry $LASTINCLUDEDID HEAD | sed -n 's/^+ //p' | head -n 50 | xargs -l1 git cvsexportcommit -c -p -v
 
