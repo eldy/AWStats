6ca56baf8e5819787c128e5ccd741701cb84bfdd
Laurent Destailleur <eldy@destailleur.fr>
2015-08-18 20:41:20 +0200

    Compatibility with Perl 5.22

--- wwwroot/cgi-bin/awstats.pl
+++ wwwroot/cgi-bin/awstats.pl
@@ -9040,12 +9040,12 @@ sub DefinePerlParsingFormat {
 		$LogFormatString =~ s/%>s/%code/g;
 		$LogFormatString =~ s/%b(\s)/%bytesd$1/g;
 		$LogFormatString =~ s/%b$/%bytesd/g;
-		$LogFormatString =~ s/\"%{Referer}i\"/%refererquot/g;
-		$LogFormatString =~ s/\"%{User-Agent}i\"/%uaquot/g;
-		$LogFormatString =~ s/%{mod_gzip_input_size}n/%gzipin/g;
-		$LogFormatString =~ s/%{mod_gzip_output_size}n/%gzipout/g;
-		$LogFormatString =~ s/%{mod_gzip_compression_ratio}n/%gzipratio/g;
-		$LogFormatString =~ s/\(%{ratio}n\)/%deflateratio/g;
+		$LogFormatString =~ s/\"%\{Referer}i\"/%refererquot/g;
+		$LogFormatString =~ s/\"%\{User-Agent}i\"/%uaquot/g;
+		$LogFormatString =~ s/%\{mod_gzip_input_size}n/%gzipin/g;
+		$LogFormatString =~ s/%\{mod_gzip_output_size}n/%gzipout/g;
+		$LogFormatString =~ s/%\{mod_gzip_compression_ratio}n/%gzipratio/g;
+		$LogFormatString =~ s/\(%\{ratio}n\)/%deflateratio/g;
 
 		# Replacement for a IIS and ISA format string
 		$LogFormatString =~ s/cs-uri-query/%query/g;    # Must be before cs-uri
