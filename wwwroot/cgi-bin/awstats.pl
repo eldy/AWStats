240cfdabbdac573651dfae0fa2228dc67b56db83
Tomaz Solc <tomaz.solc@tablix.org>
2018-06-27 14:24:57 +0200

    Ignore search phrases longer than 80 characters.

--- wwwroot/cgi-bin/awstats.pl
+++ wwwroot/cgi-bin/awstats.pl
@@ -19986,7 +19986,7 @@ s/^(cache|related):[^\+]+//
 													$param =~ s/^ +//;
 													$param =~ s/ +$//;    # Trim
 													$param =~ tr/ /\+/s;
-													if ( ( length $param ) > 0 )
+													if ( ( ( length $param ) > 0 ) and ( ( length $param ) < 80 ) )
 													{
 														$_keyphrases{$param}++;
 													}
